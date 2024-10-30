5ba57bd438eaf3e69122973a265fc0fdd3985bf4
Kathryn Hodge <blondiebytes@gmail.com>
2024-10-30 11:37:49 +0000

    Starter

--- app.py
+++ app.py
@@ -3,36 +3,40 @@ from collections import deque
 
 app = Flask(__name__)
 
-waiting_list_queue = deque()
+waiting_list = None # TODO: Add data structure
 next_id = 1
 
 @app.route('/')
 def index():
     return render_template('index.html', waiting_list=list(waiting_list_queue))
 
-@app.route('/reserve', methods=['POST'])
-def reserve():
+@app.route('/addToWaitingList', methods=['POST'])
+def add_to_waiting_list():
     global next_id
     data = request.json
     if 'name' in data:
-        reservation = {"id": next_id, "name": data['name']}
-        waiting_list_queue.append(reservation)  
+        # TODO: Create guest to add to waiting list 
+        # data['name'] holds the name of the guest to add to the list
+        guest = None
+        # TODO: Add guest to waiting list
         next_id += 1
-        return jsonify({"message": "Reservation added", "reservation": reservation}), 201
+        return jsonify({"message": "Guest added", "reservation": guest}), 201
     else:
         return jsonify({"message": "Name is required"}), 400
 
 @app.route('/waitinglist', methods=['GET'])
 def get_waiting_list():
-    return jsonify(list(waiting_list_queue))
+    # TODO: Return all the guests on the waiting_list
+    return jsonify(waiting_list)
 
 @app.route('/seatNextGuest', methods=['POST'])
-def process_reservation():
-    if waiting_list_queue:
-        processed_reservation = waiting_list_queue.popleft()  # Dequeue reservation
-        return jsonify({"message": "Reservation processed", "reservation": processed_reservation}), 200
+def seat_guest():
+    if waiting_list:
+        # TODO: Seat first guest on waiting list
+        processed_guest = None
+        return jsonify({"message": "Guest seated", "seated_guest": processed_guest}), 200
     else:
-        return jsonify({"message": "No reservations in the queue"}), 200
+        return jsonify({"message": "No guests in the queue"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)

185fec9f86683698266f45964528278d6c1ac7c3
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 22:44:32 +0000

    Starter

--- app.py
+++ app.py
@@ -1,21 +1,2 @@
-user_preferences = {
-    "language": "English",
-    "fontSize": "14px",
-    "timezone": "GMT",
-    "currency": "USD",
-    "enableLocation": False,
-    "volumeLevel": 80,
-    "dateFormat": "MM/DD/YYYY"
-}
-
-user_preferences["language"] = "Spanish"
-user_preferences["volumeLevel"] = 50
-
-user_preferences["highlightColor"] = "yellow"
-
-#del user_preferences["currency"]
-removed_item = user_preferences.pop("currency", "N/A")
-
-print(user_preferences)
-
+employee_salaries = {"Alice": 50000, "Bob": 60000}
 

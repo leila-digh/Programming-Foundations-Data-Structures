7ebb0d9d244490e71d40a9ca34683019bdd5b51d
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 23:13:21 +0000

    Starter

--- app.py
+++
@@ -1,9 +0,0 @@
-book_authors = {"1984": "George Orwell", "Pride and Prejudice": "Jane Austen", "Moby Dick": "Herman Melville"}
-
-print(book_authors.keys())
-
-print(book_authors.values())
-
-for book, author in book_authors.items():
-  print(f"{book} by {author}")
-

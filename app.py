ee6c13468195cdba67429ddc5b8c16b2ae650c9a
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 22:50:53 +0000

    starter

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-employee_salaries = {"Alice": 50000, "Bob": 60000}
-
-# get()
-print(employee_salaries.get("Charlie", "Not Found"))
-
-# try-except
-try:
-    charlie_salary = employee_salaries["Charlie"]
-except KeyError:
-    print("Charlie is still not found")
+book_authors = {"1984": "George Orwell", "Pride and Prejudice": "Jane Austen", "Moby Dick": "Herman Melville"}

e7b9bdafc8bd23762574d91178fe4eece0d371dc
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 18:24:47 +0000

    Update starter

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 
 student_books_list = [5, 3, 0, 7, 2, 4, 6, 8, 1, 3, 9, 5, 2, 6, 7, 4]
+
+# Average Calculation
+total_books = 0
+for individual_books in student_books_list:
+  total_books += individual_books
+print(total_books)
+
+average_books = total_books / len(student_books_list)
+print(f"The average number of books read per student is {average_books:.2f}")

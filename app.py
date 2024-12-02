886fe04b0eb30767af48734a043d07f98a8fe37e
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 22:44:05 +0000

    Implement

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ user_preferences = {
     "volumeLevel": 80,
     "dateFormat": "MM/DD/YYYY"
 }
+
+user_preferences["language"] = "Spanish"
+user_preferences["volumeLevel"] = 50
+
+user_preferences["highlightColor"] = "yellow"
+
+#del user_preferences["currency"]
+removed_item = user_preferences.pop("currency", "N/A")
+
+print(user_preferences)
+
+

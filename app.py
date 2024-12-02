9c5f47d67a8ac07c254d21eaff004ef970c9cd9e
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 18:35:52 +0000

    implement

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 
 student_books_list = [5, 3, 0, 7, 2, 4, 6, 8, 1, 3, 9, 5, 2, 6, 7, 4]
 
+# First student read 3 more books
+# Second student read 1 more book
+# Last student read 2 more books
+
+student_books_list[0] = 8
+student_books_list[1] += 1 #student_books_list[1] = student_books_list[1] + 1
+student_books_list[-1] += 2
+
+student_books_list.append(3)
+student_books_list.append(5)
+
 # Average Calculation
 total_books = 0
 for individual_books in student_books_list:

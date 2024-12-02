750f0bca5b6d8078ae2fce690ee1e45acdb94996
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 18:11:20 +0000

    record

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ num_of_students = len(student_books_list)
 print(f"There are {num_of_students} students in the class.")
 
 # sum / num of students
+
+item_at_index_3 = student_books_list[3]
+
+#Index Error: list index out of range
+# print(student_books_list[20])
+
+item_three_from_back = student_books_list[-3]
+print(item_three_from_back)
+
+total_books = 0
+for individual_book in student_books_list:
+  total_books += individual_book
+print(total_books)
+
+average_books = total_books / num_of_students
+print(f"The average number of books read per student is {average_books:.2f}")
+

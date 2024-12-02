e49c71baafcfb44444ca52fbc97179451ba36a20
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 17:54:38 +0000

    Implement

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 
 # Problem Statement: Compute the average number of books each student has read this year
 
+student_books_list = [5, 3, 0, 7, 2, 4, 6, 8, 1, 3, 9, 5, 2, 6, 7, 4]
+
+num_of_students = len(student_books_list)
+print(f"There are {num_of_students} students in the class.")
+
+# sum / num of students

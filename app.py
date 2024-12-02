518c5e2dbf6aa5bb17d34b81c445bf6c95312311
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 21:44:05 +0000

    Starter

--- app.py
+++
@@ -1,9 +0,0 @@
-my_list = [1, 7, 3]
-
-print(sorted(my_list, reverse=True))
-
-student_grades = [('Sarah', 89), ('Rebecca', 82), ('Matt', 91)]
-
-print(sorted(student_grades, key=lambda x: x[1], reverse=True))
-
-# Extensive comparisons and checks make sorting computationally expensive

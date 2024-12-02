26b0de509ba01dc8bd51b2e684cfa351cbec17f8
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 19:09:51 +0000

    starter

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-# Multidimensional List (Nested List)
-# - Store multiple lists within a single list
-
-seating_chart = [
-  ["Sarah", "Claire", "Ben", "Taylor", "Eva"],
-  ["Frankie", "George", "Lindsey", "Izzy", "Jack"],
-  ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
-  ["Chad", "April", "Matt", "Thomas", "Penny"]
-]
-
-# print(seating_chart[2][1])
-
-for i, row in enumerate(seating_chart):
-  for j, student_name in enumerate(row):
-    print(f"{student_name} is in row {i+1}, seat {j+1}")
+# Tuples

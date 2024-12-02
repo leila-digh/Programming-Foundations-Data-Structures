eb2adf496c1e9bf57fba17d35569b30a9fe88e71
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-02 22:50:15 +0000

    Implement

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 employee_salaries = {"Alice": 50000, "Bob": 60000}
 
+# get()
+print(employee_salaries.get("Charlie", "Not Found"))
+
+# try-except
+try:
+    charlie_salary = employee_salaries["Charlie"]
+except KeyError:
+    print("Charlie is still not found")

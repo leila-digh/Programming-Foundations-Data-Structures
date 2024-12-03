b9f725068304cf87116d0fd1908b1a30c082500b
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-03 17:25:10 +0000

    Starter

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
-# Sets are mutable
-fruits = {"apple", "banana", "cherry"}
-fruits.add("orange")
-print(fruits)
-
-fruits.update(["mango", "grape"])
-print(fruits)
-
-# remove(item) - can raise KeyError
-# discard(item) - does not raise KeyError
-
-fruits.remove("banana")
-fruits.discard("tomato")
-print(fruits)
+set_A = {10, 20, 30, 40, 50}
+set_B = {30, 40, 50, 60, 70}

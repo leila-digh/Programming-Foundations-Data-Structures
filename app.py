8bd8a8ce3381f0848119e4f7e051a2296bfa0af3
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-03 17:17:17 +0000

    Implement

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-
+# Sets are mutable
 fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+print(fruits)
+
+fruits.update(["mango", "grape"])
+print(fruits)
+
+# remove(item) - can raise KeyError
+# discard(item) - does not raise KeyError
+
+fruits.remove("banana")
+fruits.discard("tomato")
+print(fruits)

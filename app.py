fd1cd96ed5b88066f99496204138c8f52e73202c
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-03 17:08:24 +0000

    starter

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-# Primary Colors
 
-primary_colors = set(["red", "blue", "yellow"])
-
-color = "green"
-
-if color in primary_colors:
-  print(color + " is a primary color")
-else:
-  print(color + " is not a primary color")

886c2cd6e4744a0224591d23aea2ccc808144b68
Kathryn Hodge <blondiebytes@gmail.com>
2024-12-04 17:51:09 +0000

    End code

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, request, render_template
-
+from collections import deque
 app = Flask(__name__)
 
-waiting_list = None # TODO: Add data structure
+waiting_list = deque()
 next_id = 1
 
 @app.route('/')
 def index():
     # TODO: Return all the guests on the waiting_list
-    return render_template('index.html', waiting_list=waiting_list)
+    return render_template('index.html', waiting_list=list(waiting_list))
 
 @app.route('/addToWaitingList', methods=['POST'])
 def add_to_waiting_list():
@@ -17,9 +17,10 @@ def add_to_waiting_list():
     if 'name' in data:
         # TODO: Create guest to add to waiting list 
         # data['name'] holds the name of the guest to add to the list
-        guest = None
+        guest = { "id" : next_id, "name" : data['name']}
         # TODO: Add guest to waiting list
         next_id += 1
+        waiting_list.append(guest)
         return jsonify({"message": "Guest added", "reservation": guest}), 201
     else:
         return jsonify({"message": "Name is required"}), 400
@@ -28,7 +29,7 @@ def add_to_waiting_list():
 def seat_guest():
     if waiting_list:
         # TODO: Seat first guest on waiting list
-        processed_guest = None
+        processed_guest = waiting_list.popleft()
         return jsonify({"message": "Guest seated", "seated_guest": processed_guest}), 200
     else:
         return jsonify({"message": "No guests in the queue"}), 200
